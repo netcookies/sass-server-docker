959bc7da396300579b7fe967f90600463cfda8c3
Isulew <netcookies@gmail.com>
2017-03-13 22:32:47 +0800

    update

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ src_path= `pwd`/src
 
 docker run -d \
     -p $http_port:8080 \
-    -p $reserve_proxy_port = 8000 \
-    -p $admin_port = 3001 \
+    -p $reserve_proxy_port:8000 \
+    -p $admin_port:3001 \
     -v $public_path:/app/web/public \
     -v $src_path:/app/web/src \
     sass-server-docker
